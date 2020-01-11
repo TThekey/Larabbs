703f5419af0501db15506c273d768e4515ecb5e7
TThekey <1026261410@qq.com>
2020-01-11 22:33:52 +0800

    修改配置文件

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravqqq
+                    Larav
                 </div>
 
                 <div class="links">
